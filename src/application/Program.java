dfd1e6b4ccd73e9782182de7d60ca5db96c91779
Leonardodev99 <pequenotiago2016@gmail.com>
2023-10-26 12:21:39 +0100

    Criada a classe de produtos e associou-se com a classe principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,33 @@
 package application;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Leonardo desenvolvedor!");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+		
+		System.out.print("Enter with path file input: ");
+		String path = sc.nextLine();
+		
+		File sourceFile = new File(path);
+		
+		String sourceFolder = sourceFile.getParent();
+		
+		boolean subFolder = new File(sourceFolder+"\\out").mkdir();
+		
+		
+		
+		
+		sc.close();
 
 	}
 
